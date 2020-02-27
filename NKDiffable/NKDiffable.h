440ae72a8de04ecf1d2d7722306ea6b8fa6e8ba6
Mihai Fratu <zeusent@msn.com>
2020-02-27 19:30:24 +0200

    Initial Commit

---
+++ NKDiffable/NKDiffable.h
@@ -0,0 +1,19 @@
+//
+//  NKDiffable.h
+//  NKDiffable
+//
+//  Created by Mihai Fratu on 27/02/2020.
+//  Copyright © 2020 Mihai Fratu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NKDiffable.
+FOUNDATION_EXPORT double NKDiffableVersionNumber;
+
+//! Project version string for NKDiffable.
+FOUNDATION_EXPORT const unsigned char NKDiffableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NKDiffable/PublicHeader.h>
+
+
